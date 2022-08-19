1ffabf92ec9684b717c0935cdd7b0d7fc3b51f97
Elanthamil <84515883+Elan777@users.noreply.github.com>
2022-08-19 10:11:52 +0530

    Update DataStructures.py

--- DataStructures.py
+++ DataStructures.py
@@ -1,5 +1,14 @@
+#Finding Area of the Circle
 # r is the Radius of the circle
 
 r=float(input("Input the Radius of the circle: "))
 Area = (22/7)*r*r
 print("The area of the circle with radius r is :", Area)
+
+
+
+
+#Accept a filename from the user and print the extension of that
+Filename=input("Input the filename: ")
+File_extension=Filename.split(".")
+print("The extension of the file is: ", repr(File_extension[-1]))
